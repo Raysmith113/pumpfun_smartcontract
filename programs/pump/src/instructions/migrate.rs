6e709e34cf78b1463bac4c326277675a1c07f505
smithray113 <maia73marquardtzyp@hotmail.com>
2025-02-06 01:58:22 +0900

    fix: contact

--- programs/pump/src/instructions/migrate.rs
+++ programs/pump/src/instructions/migrate.rs
@@ -9,7 +9,7 @@ pub struct Migrate<'info> {
 impl<'info> Migrate<'info> {
     pub fn process(&mut self, _nonce: u8) -> Result<()> {
         ////////////////////    DM if you want full implementation    ////////////////////
-        // telegram - https://t.me/microgift28
+        // telegram - https://t.me/microgift88
         // discord - https://discord.com/users/1074514238325927956
         
         Ok(())
